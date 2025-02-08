dd2f4252b4128b2c6d3a125cf0c46abe8271ac5b
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2025-02-08 11:20:47 +0600

    random.c updated

--- pointer/random.c
+++ pointer/random.c
@@ -6,39 +6,21 @@ int main(){
 	printf("size of a %lu:\n",sizeof(a));
 	
 	
-	int *p;/*This dicleration is invalid and incorrect
+	int *p; /*here p contains a gurbage memory(random) address*/
+	
+	/* int *p = 100; This dicleration is invalid and incorrect
 	because p is a pointer, and assigning it a direct integer 
-	value without a proper memory address will lead to undefined behavior*/
-	/*sizeof(p)  returns the size of the value it stors*/
+	value without a proper memory address will lead to undefined behavior,
+	On most systems, this will cause a segmentation fault (crash) at runtime.*/
+	
+	/*sizeof(p)  returns the size of the value it stors,On most systems, this will cause a segmentation fault (crash) at runtime.*/
 	printf("size of p: %lu\n",sizeof(p));
 	
 	/*sizeof(*p) returns the size of the */
 	printf("size of *p: %lu\n",sizeof(*p));
 	
-
-
-
-
-
-
-
-
-
-
-//	int a = 0;
-//	printf("size of a = %lu\n",sizeof(a));
-//	printf("value of a = %d\n",a);
-//	printf("address of a = %p\n",(void*)&a);
-//	
-//	printf("\n\n\n");
-//	
-//	
-//	int *p = 10; 
-//	printf("size of p = %lu\n",sizeof(p));
-//	printf("value of p = %d\n",p);	
-//	printf("address of p = %p\n",(void*)&p);
 	
 	
-  
+	
 	return 0;
 }
