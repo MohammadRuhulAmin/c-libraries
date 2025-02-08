fcd01cab6edd91c95af78fe0e04d9a29592fb347
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2025-02-08 11:18:00 +0600

    random file updated

--- pointer/random.c
+++ pointer/random.c
@@ -1,20 +1,44 @@
 #include<stdio.h>
 
 int main(){
+	int a = 10;
+	printf("address of a: %p\n",(void*)&a);
+	printf("size of a %lu:\n",sizeof(a));
+	
+	
+	int *p;/*This dicleration is invalid and incorrect
+	because p is a pointer, and assigning it a direct integer 
+	value without a proper memory address will lead to undefined behavior*/
+	/*sizeof(p)  returns the size of the value it stors*/
+	printf("size of p: %lu\n",sizeof(p));
+	
+	/*sizeof(*p) returns the size of the */
+	printf("size of *p: %lu\n",sizeof(*p));
+	
 
-	int a = 0;
-	printf("size of a = %lu\n",sizeof(a));
-	printf("value of a = %d\n",a);
-	printf("address of a = %p\n",(void*)&a);
+
+
+
+
+
+
+
+
+
+//	int a = 0;
+//	printf("size of a = %lu\n",sizeof(a));
+//	printf("value of a = %d\n",a);
+//	printf("address of a = %p\n",(void*)&a);
+//	
+//	printf("\n\n\n");
+//	
+//	
+//	int *p = 10; 
+//	printf("size of p = %lu\n",sizeof(p));
+//	printf("value of p = %d\n",p);	
+//	printf("address of p = %p\n",(void*)&p);
 	
-	printf("\n\n\n");
 	
-	
-	int *p = 10; 
-	printf("size of p = %lu\n",sizeof(p));
-	printf("value of p = %d\n",p);	
-	printf("address of p = %p\n",(void*)&p+1);
-		
   
 	return 0;
 }
