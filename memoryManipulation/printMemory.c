a0df2a555336f7341a3270f8e8681c1021d768c6
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2025-02-10 12:32:16 +0600

    Byte printing of a variable

--- memoryManipulation/printMemory.c
+++ memoryManipulation/printMemory.c
@@ -1,23 +1,31 @@
 #include<stdio.h>
-void printBinary(unsigned char byte){
-	for(int i = 7;i>=0;i--){
-		printf("%d",(byte >> 1)&1);
-	
-	}
 
-}
-
-int main(int argc, char *argv[]){
+int main(){
 	int a = 10;
-	unsigned char *ptr = (unsigned char*)&a;
-	/* a char is 1 byte (8 bits), making it perfect for reading individual bytes of memory */
-	printf("Memory Representation of int a = %d:\n",a);
-	for(size_t i = 0; i < sizeof(a);i++){
-		printf("Byte Hexa  %zu:0x%02X  Binary :%zu  \n",i,ptr[i],printBinary(ptr[i]));
+	unsigned char *ptr_a = (unsigned char*)&a;
+	// %zu is used to print size_t
+	printf("\nMemory Representation of int a = %d\n",a);
+	for(size_t i = 0;i<sizeof(a);i++){
+		printf("Byte Hexa %zu: 0x%02X, address :%p \n",i,ptr_a[i],(void*) ptr_a);
 	
 	}
+	printf("\n Memory Representation of int b = 20 \n");
+	int b = 100;
+	unsigned char *ptr_b = (unsigned char*)&b;
+	for(size_t i= 0; i<sizeof(b);i++){
+		printf("Byte Hexa %zu: 0x%02X, address :%p \n",i,ptr_b[i],(void*) ptr_b);
+	}
+
+	int c = 30;
+	unsigned char *ptr_c = (unsigned char*) &c;
+	printf("\nManually printing of int c = 30\n");
+	printf("index 0 = 0x%2X\n",ptr_c[0]);
+	printf("index 1 = 0x%2X\n",ptr_c[1]);
+	printf("index 2 = 0x%2X\n",ptr_c[2]);
+	printf("index 3 = 0x%2X\n",ptr_c[3]);
+
+
 	return 0;
-	
 }
 
 
